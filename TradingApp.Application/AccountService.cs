52ac510675e867de8f7efaff059e969470b3474b
Sam <samuelmatthews96@gmail.com>
2025-11-05 16:20:44 +0000

    Add logging to AccountService

--- TradingApp.Application/AccountService.cs
+++ TradingApp.Application/AccountService.cs
@@ -6,10 +6,12 @@ namespace TradingApp.Application;
 public class AccountService : IAccountService
 {
     private readonly IAccountRepository _accountRepository;
+    private readonly ILogger<AccountService> _logger;
     
-    public AccountService(IAccountRepository accountRepository)
+    public AccountService(IAccountRepository accountRepository, ILogger<AccountService> logger)
     {
         _accountRepository = accountRepository;
+        _logger = logger;
     }
 
     public async Task<Account> Create(string name, decimal amount)
@@ -19,24 +21,32 @@ public class AccountService : IAccountService
         {
             await _accountRepository.AddAccount(account);
         }
+        
+        _logger.LogInformation("Account created successfully: {AccountId}", account.Id);
         return account;
     }
 
     public async Task<Account?> Retrieve(string name)
     {
+        _logger.LogInformation("Account retrieval requested: {AccountName}", name);
         var account = await _accountRepository.GetAccount(name);
+        
         return account;
     }
 
     public async Task<Account> Deposit(string accountId, decimal amount)
     {
         var account = await _accountRepository.Deposit(accountId, amount);
+        
+        _logger.LogInformation("Deposit amount {Amount} successful for: {AccountId}", amount, account.Id);
         return account;
     }
     
     public async Task<Account> Withdraw(string accountId, decimal amount)
     {
         var account = await _accountRepository.Withdraw(accountId, amount);
+        
+        _logger.LogInformation("Withdrawal amount {Amount} successful for: {AccountId}", amount, account.Id);
         return account;
     }
 }
